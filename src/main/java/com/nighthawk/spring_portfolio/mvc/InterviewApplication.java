9099c384be871001089841544b121c533793a2ba
isabelle926 <gunawanisabelle@gmail.com>
2024-02-07 09:56:09 -0800

    minor edits

--- src/main/java/com/nighthawk/spring_portfolio/mvc/InterviewApplication.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/InterviewApplication.java
@@ -3,16 +3,11 @@ package com.nighthawk.spring_portfolio.mvc;
 import com.nighthawk.spring_portfolio.mvc.user.User;
 import com.nighthawk.spring_portfolio.mvc.user.UserService;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InterviewApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewApplication.class, args);
-	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner(
